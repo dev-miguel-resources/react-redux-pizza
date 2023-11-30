186b3135199fa65c9d6377e0ca9d4af2a44885a2
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-11-30 09:18:43 -0300

    services api pizza completed

---
+++ src/route.js
@@ -0,0 +1,10 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+
+    },
+    {
+        
+    }
+]);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./index.html', './src/**/*.{jsx}'],
+  theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+    // pending extends
+  },
+  plugins: [],
+}
